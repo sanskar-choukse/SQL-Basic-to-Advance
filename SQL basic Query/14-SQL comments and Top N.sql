5f38126d927f261c60823ab03c86ac81f6e72afc
sanskar chouksey <sanskarchoukse468@gmail.com>
2025-08-15 14:40:09 +0530

    Top n Data

---
+++ SQL basic Query/14-SQL comments and Top N.sql
@@ -0,0 +1,19 @@
+
+--single line comment
+Select * from dbo.employees
+
+/*
+multiple
+line
+comments
+*/
+Select * from dbo.employees
+
+-- top 2 dataset have to see
+select top 2 * from employees
+
+select top 5 * from employees
+
+select top 3 Firstname,Lastname,Department from employees
+
+select top 5 Salary from employees
